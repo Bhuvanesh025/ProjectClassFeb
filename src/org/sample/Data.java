4e0ca97cac63c8ddcc8f52e55fbe73d52f80dd0b
Bhuvanesh025 <ne.bhuvanesh@gmail.com>
2021-02-18 09:15:39 +0530

    Person2 added some lines

--- src/org/sample/Data.java
+++ src/org/sample/Data.java
@@ -1,5 +1,11 @@
 package org.sample;
 
 public class Data {
+	
+	public static void main(String[] args) {
+		
+		System.out.println("Person2 is adding his code");
+		System.out.println("Person2 done his work");
+	}
 
 }
