9adbc1fad1a926fbbec36267e0d1c7bf7cabf300
Bhuvanesh025 <ne.bhuvanesh@gmail.com>
2021-02-18 09:01:00 +0530

    Person added his code

--- src/org/sample/Data.java
+++ src/org/sample/Data.java
@@ -1,5 +1,11 @@
 package org.sample;
 
 public class Data {
+	
+public static void main(String[] args) {
 
+	System.out.println("Person 1 added some lines");
+	System.out.println("Work done by Person 1");
+	
+}	
 }
